8740dd971115b650727129952229fd111f0d54ab
Colton Hurst <colton@coltonhurst.com>
2018-05-22 16:29:56 -0400

    Added dev playbook sql notes

---
+++ sql/dev-playbook/dpb-2.sql
@@ -0,0 +1,20 @@
+-- Developer's T-SQL Playbook
+-- Section 2
+
+/* ----- Tables ----- */
+
+--create table Users(
+--	email varchar(50)
+--);
+
+--drop table Users;
+
+--create table Users(
+--	Id integer primary key
+--);
+
+/* ----- GUIDs ----- */
+
+--create table Users(
+--	Id uniqueidentifier primary key
+--);
